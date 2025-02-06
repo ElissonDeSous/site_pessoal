3983a903515c2381d887858fe779b5a99a205149
ElissonDeSous <elissondsousa@gmail.com>
2025-02-06 11:57:33 -0300

    alterando topo

--- src/app/components/topo/topo.tsx
+++ src/app/components/topo/topo.tsx
@@ -13,7 +13,7 @@ export default function Topo(){
      <header className="h-[70px] fixed w-full flex  bg-black text-white items-center justify-around ">
             <div>
                 <Link href="/">
-                   <Image className='md:w-[70px] w-[100px]' src = {Logo} width={100} alt='Logo da empresa' />
+                   <Image className='md:w-[70px] w-[70px]' src = {Logo} width={100} alt='Logo da empresa' />
                 </Link>
                 
             </div>
@@ -30,7 +30,7 @@ export default function Topo(){
               
 
                 <div onClick={menu} className='md:hidden flex '>
-            <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="80" height="128" viewBox="0 0 128 128">
+            <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="50" height="128" viewBox="0 0 128 128">
                 <path fill="#fff" d="M64 14A50 50 0 1 0 64 114A50 50 0 1 0 64 14Z"></path><path fill="#444b54" d="M64,117c-29.2,0-53-23.8-53-53s23.8-53,53-53s53,23.8,53,53S93.2,117,64,117z M64,17c-25.9,0-47,21.1-47,47s21.1,47,47,47s47-21.1,47-47S89.9,17,64,17z"></path><path fill="#444b54" d="M86.5 52h-45c-1.7 0-3-1.3-3-3s1.3-3 3-3h45c1.7 0 3 1.3 3 3S88.2 52 86.5 52zM86.5 67h-45c-1.7 0-3-1.3-3-3s1.3-3 3-3h45c1.7 0 3 1.3 3 3S88.2 67 86.5 67z"></path><g><path fill="#444b54" d="M86.5,82h-45c-1.7,0-3-1.3-3-3s1.3-3 3-3h45c1.7 0 3,1.3 3,3S88.2,82,86.5,82z"></path></g>
              </svg>
 
@@ -42,10 +42,6 @@ export default function Topo(){
                         <Link className='font-bold hover:text-red-500' href='/servico'>Quem Somos</Link>
                         <Link className='font-bold hover:text-red-500' href='/servico'>Contato</Link>
                     </div>
-                
-
-                
-             
      </header>
     )
 }
